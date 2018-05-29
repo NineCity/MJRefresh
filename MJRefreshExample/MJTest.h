ce358d65efb5ad2f39d2c22b8bb8f0a08774651a
童城 <1171169229@qq.com>
2018-05-29 23:48:38 +0800

    add Test

---
+++ MJRefreshExample/MJTest.h
@@ -0,0 +1,13 @@
+//
+//  MJTest.h
+//  MJRefreshExample
+//
+//  Created by 童城 on 2018/5/29.
+//  Copyright © 2018年 小码哥. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MJTest : NSObject
+
+@end
---
+++ MJRefreshExample/MJTest.m
@@ -0,0 +1,13 @@
+//
+//  MJTest.m
+//  MJRefreshExample
+//
+//  Created by 童城 on 2018/5/29.
+//  Copyright © 2018年 小码哥. All rights reserved.
+//
+
+#import "MJTest.h"
+
+@implementation MJTest
+
+@end
